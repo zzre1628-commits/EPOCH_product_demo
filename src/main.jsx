a7d0fa059d4a3571c170dbeb587cb6d30d16af2a
zzre1628-commits <zzre1628@gmail.com>
2026-01-14 11:22:21 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // 确保这里的文件名大小写与你的 src 文件夹里完全一致
 import './index.css'
 
+// 这里的 App 组件必须被正确 import 进来，才能被渲染
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
